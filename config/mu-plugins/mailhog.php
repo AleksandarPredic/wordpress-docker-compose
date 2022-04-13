e8c2f8b90edad3297e860aab8456fd28336d0b59
AleksandarPredic <aleksandar.predic@gmail.com>
2022-04-13 17:26:45 +0200

    Added 9999999 as a priority for mu-plugin mailhog.php to make it override any other plugin priority so we don't send any emails from the localhost but redirect them to local mailhog

--- config/mu-plugins/mailhog.php
+++ config/mu-plugins/mailhog.php
@@ -1,11 +1,17 @@
 <?php
 /**
  * Force WP to send emails to mailhog
+ *
+ * Set to run last to override any possible plugins
  */
-add_action('phpmailer_init', 'predicSetupMailhog');
+add_action('phpmailer_init', 'predicSetupMailhog', 9999999);
 function predicSetupMailhog($phpmailer)
 {
     $phpmailer->Host = 'mailhog';
     $phpmailer->Port = 1025;
     $phpmailer->IsSMTP();
+    $phpmailer->Username = '';
+    $phpmailer->Password = '';
+    $phpmailer->SMTPAuth = false;
+    $phpmailer->SMTPSecure = 'TLS';
 }
